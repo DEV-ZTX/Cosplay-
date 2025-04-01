75b4b2c55dd2fab9f44135f3466206c2e5fa4f67
DEV-ZTX <sucidaldazai777@gmail.com>
2025-04-01 18:24:54 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -7,7 +7,9 @@ from html import escape
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup 
 from telegram import Update 
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filters
-from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER from shivu.modules import ALL_MODULES
+from shivu import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu 
+from shivu import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER 
+from shivu.modules import ALL_MODULES
 
 locks = {} 
 message_counters = {} 
